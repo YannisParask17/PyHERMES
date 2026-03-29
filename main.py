cf911f1cbc9828c43a30a57e233ae25ffdd8a550
YannisParask17 <j.parask7@gmail.com>
2026-03-29 19:45:39 +0200

    Fundamental structuring of project folder

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    print('\n----------------- P y H E R M E S -----------------')
+
+    print('[LOG] Collecting job postings from the WEB')
+
+    print('[LOG] Saving found job postings to DB')
+
+    print('[LOG] Notifiying selected users')
+
+    print('----------------- P y H E R M E S -----------------\n')
+
+
+
+
+
+if __name__ == '__main__':
+    main()
